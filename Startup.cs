65b6de24af46821de76fa608d117a344e018f8ce
Alexandr <43495027+Popuzikan@users.noreply.github.com>
2021-03-03 22:45:09 +0300

    Startup-Commit

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,11 @@
- using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shop.Data;
 using Shop.Data.Interfaces;
-using Shop.Data.Mocks;
 using Shop.Data.Models;
 using Shop.Data.Repository;
 
